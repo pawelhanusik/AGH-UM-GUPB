e6943e47df40d47d1177bb764c165b3f2c4d0cc1
Pawel Hanusik <39835401+pawelhanusik@users.noreply.github.com>
2023-12-10 02:21:03 +0100

    Test Axe

--- gupb/controller/aragorn/constants.py
+++ gupb/controller/aragorn/constants.py
@@ -5,7 +5,7 @@ USE_PF_CACHE = False
 OPTIMIZE_PF = False
 INFINITY: int = 99999999999
 WEAPON_HIERARCHY = {
-    "axe" : 4,
+    "axe" : 6,
     "sword" : 4,
     "bow_loaded" : 1,
     "bow_unloaded" : 1,
